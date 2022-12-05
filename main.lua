d579d4788b2d78d0167d32d0f6bc970576b198f5
Paul Rijneveld <paulrijneveld@gmail.com>
2022-12-05 21:26:30 +0000

    Implement example reading

--- main.lua
+++ main.lua
@@ -1,3 +1,5 @@
+local num_positions = 3
+
 Move = {num = 0, from = 0, to = 0}
 function Move:new (o,num,from,to)
    o = o or {}
@@ -13,25 +15,44 @@ function Move:printMove ()
    print('Move ' .. self.num .. ' from ' .. self.from .. ' to ' .. self.to)
 end
 
-Row = {items = {}}
-function Row:printRow ()
-    print('Row')
-    for item in self.items do
-        print('  ' .. item)
-    end
-end
-
 function Read_move(line)
     local tokens = {}
     for token in line:gmatch("%w+") do table.insert(tokens, token) end
     return Move:new(nil, tokens[2],tokens[4],tokens[6])
 end
 
+Row = {items = {}}
+function Row:new (o,items)
+   o = o or {}
+   setmetatable(o, self)
+   self.__index = self
+   self.items = items or {}
+   return o
+end
+
+function Row:printRow ()
+    print('Row')
+    for i=1,3 do
+        print(i .. '  "' .. self.items[i] .. '"')
+    end
+end
+
 function Read_row(line)
-    -- ------------ Continue here
-    local tokens = {}
-    for token in line:gmatch("%w+") do table.insert(tokens, token) end
-    return Move:new(nil, tokens[2],tokens[4],tokens[6])
+    local row_items = {}
+    for i=1,num_positions do
+        local index = i*4-2
+        row_items[i] = Read_crate(line,index)
+    end
+    return Row:new(nil, row_items)
+end
+
+function Read_crate(line, pos)
+    local c = string.sub(line, pos, pos)
+    if c == '' then
+       return ' '
+    else
+        return c
+    end
 end
 
 function Lines_from(file)
@@ -46,16 +67,28 @@ function string.starts(String,Start)
    return string.sub(String,1,string.len(Start))==Start
 end
 
-local lines = Lines_from('Input.txt')
+local rows = {}
+local moves = {}
 
-for k,v in pairs(lines) do
-  if string.starts(v, 'move')
-  then
-    local m = Read_move(v)
-    m:printMove()
-  elseif string.starts(v, '[')
-  then
-    local r = Read_row(v)
-    r:printRow()
-  end
+function Read_start()
+    local input = Lines_from('Example.txt')
+    local stacks_finished = false
+    for _,v in pairs(input) do
+
+      if string.starts(v, ' 1') then
+         stacks_finished = true
+      end
+
+      if stacks_finished and string.starts(v, 'move') then
+        local m = Read_move(v)
+        m:printMove()
+        moves[#moves + 1] = m
+      elseif not stacks_finished then
+        local r = Read_row(v)
+        r:printRow()
+        rows[#rows + 1] = r
+      end
+    end
 end
+
+Read_start()
