565030d93cdbd81d78e7e78ade2dc12874f3d098
Paul Rijneveld <paulrijneveld@gmail.com>
2022-12-06 22:21:33 +0000

    Find solution for part 1

--- main.lua
+++ main.lua
@@ -1,5 +1,10 @@
-local num_positions = 3
+local num_positions = 9
 
+local rows = {}
+local moves = {}
+local columns = {}
+
+-- Move
 Move = {num = 0, from = 0, to = 0}
 function Move:new (o,num,from,to)
    o = o or {}
@@ -18,9 +23,10 @@ end
 function Read_move(line)
     local tokens = {}
     for token in line:gmatch("%w+") do table.insert(tokens, token) end
-    return Move:new(nil, tokens[2],tokens[4],tokens[6])
+    return Move:new{num=tonumber(tokens[2]),from=tonumber(tokens[4]),to=tonumber(tokens[6])}
 end
 
+-- Row
 Row = {items = {}}
 function Row:new (o,items)
    o = o or {}
@@ -31,10 +37,54 @@ function Row:new (o,items)
 end
 
 function Row:printRow ()
-    print('Row')
+    io.write('Row ')
     for i=1,3 do
-        print(i .. '  "' .. self.items[i] .. '"')
+        io.write('(' .. i .. ':"' .. self.items[i] .. '") ')
     end
+    print()
+end
+
+-- Column
+Column = {num=0, items = {}}
+function Column:new (o,items)
+   o = o or {}
+   setmetatable(o, self)
+   self.__index = self
+   self.num = num
+   self.items = items or {}
+   return o
+end
+
+function Column:addCrate (crate)
+    self.items[#self.items + 1] = crate
+end
+
+function Column:removeCrate ()
+    local crate = self.items[#self.items]
+    table.remove(self.items,#self.items)
+    return crate
+end
+
+function Column:removeMulti(n)
+    local toMove = {}
+    for i=1,n do
+        toMove[i] = self:removeCrate()
+    end
+    return toMove
+end
+
+function Column:addMulti(crates)
+    for i=1,#crates do
+        self:addCrate(crates[i])
+    end
+end
+
+function Column:printColumn ()
+    io.write('Column ' .. self.num .. ':')
+    for i,v in ipairs(self.items) do
+        io.write(v)
+    end
+    print()
 end
 
 function Read_row(line)
@@ -43,7 +93,7 @@ function Read_row(line)
         local index = i*4-2
         row_items[i] = Read_crate(line,index)
     end
-    return Row:new(nil, row_items)
+    return Row:new{items=row_items}
 end
 
 function Read_crate(line, pos)
@@ -67,11 +117,13 @@ function string.starts(String,Start)
    return string.sub(String,1,string.len(Start))==Start
 end
 
-local rows = {}
-local moves = {}
+function MoveCrates(move)
+    local toMove = columns[move.from]:removeMulti(move.num)
+    columns[move.to]:addMulti(toMove)
+end
 
 function Read_start()
-    local input = Lines_from('Example.txt')
+    local input = Lines_from('Input.txt')
     local stacks_finished = false
     for _,v in pairs(input) do
 
@@ -81,14 +133,44 @@ function Read_start()
 
       if stacks_finished and string.starts(v, 'move') then
         local m = Read_move(v)
-        m:printMove()
         moves[#moves + 1] = m
       elseif not stacks_finished then
         local r = Read_row(v)
-        r:printRow()
         rows[#rows + 1] = r
       end
+    end
+
+    local num_columns = #rows[1].items
+    for i=1,num_columns do
+        columns[i] = Column:new{num=i,items={}}
+    end
+
+    for i=(#rows),1,-1 do
+        for l,crate in ipairs(rows[i].items) do
+            if crate ~= ' ' then
+                columns[l]:addCrate(crate)
+            end
+        end
     end
 end
 
 Read_start()
+
+print('Columns')
+for k,column in pairs(columns) do
+    print(k)
+    columns[k]:printColumn()
+end
+
+print()
+print('Moves')
+for i,move in ipairs(moves) do
+    move:printMove()
+    MoveCrates(move)
+end
+
+for i,column in ipairs(columns) do
+    print(i)
+    column:printColumn()
+end
+
